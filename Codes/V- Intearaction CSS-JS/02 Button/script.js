49e5537607d541c5282537b3b3aebbf0740476be
bassemSeddik <bassem.seddik@gmail.com>
2021-01-21 19:38:57 +0100

    21 January update
    The Presentation is now on V5. Speacially by the additoin of the exercices for each chapter. Also the codes have been updated

---
+++ Codes/V- Intearaction CSS-JS/02 Button/script.js
@@ -0,0 +1,27 @@
+let monBouton = document.querySelector('button');
+let monTitre = document.querySelector('h1');
+monBouton.addEventListener('click', ChangerTitre);
+
+function ActiverBouton(){
+    ChangerTitre();
+}
+
+
+monBouton.addEventListener('click', function() {
+  ChangerTitre();
+});
+
+
+if (!localStorage.getItem('champTitre')) {
+    ChangerTitre();
+} else {
+    let t2 = localStorage.getItem('champTitre');
+    monTitre.textContent = 'Titre: ' + t2;
+}
+
+
+function ChangerTitre() {
+    let t = prompt('Veuillez un titre.');
+    localStorage.setItem('champTitre', t);
+    monTitre.textContent = 'Titre: ' + t ;
+}
